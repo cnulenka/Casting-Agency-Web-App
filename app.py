0abe7e37e855162901af35370e9497bf78cf9008
cnulenka <sinulenka001@gmail.com>
2021-03-06 22:43:10 +0530

    added get actors endpoint

--- app.py
+++ app.py
@@ -7,11 +7,41 @@ from flask import (
     redirect,
     url_for,
     abort,
+    jsonify,
 )
+from flask_cors import CORS
 from models import *
 
 app = Flask(__name__)
 setup_db(app)
+CORS(app)
+
+# CORS Headers
+@app.after_request
+def after_request(response):
+    response.headers.add(
+        "Access-Control-Allow-Headers", "Content-Type,Authorization"
+    )
+    response.headers.add(
+        "Access-Control-Allow-Methods", "GET,POST,DELETE,PATCH,OPTIONS"
+    )
+    return response
+
+
+@app.route("/actors", methods=["GET"])
+def get_actors():
+	try:
+		actors = Actor.query.order_by(Actor.id).all()
+		formatted_actors = [actor.format() for actor in actors]
+
+		return jsonify({
+			"success": True,
+			"actors": formatted_actors
+		}), 200
+	except Exception as error:
+        print(error)
+
+
 
 if __name__ == "__main__":
     app.run()
