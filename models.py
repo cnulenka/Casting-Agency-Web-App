0ae442a99bdeb0eb8e8a358f214e445b2450f339
cnulenka <sinulenka001@gmail.com>
2021-03-06 18:00:23 +0530

    added actors model

--- models.py
+++ models.py
@@ -45,5 +45,36 @@ class Movie(db.Model):
         return {
             "id": self.id,
             "title": self.title,
-            "release_date": self.release_date,
+            "release_date": self.release_date
+        }
+
+class Actor(db.Model):
+	__tablename__ = "actors"
+	id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String, nullable=False)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "age": self.age,
+            "gender":self.gender
         }
